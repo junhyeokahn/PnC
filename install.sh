22af1748d46414422ea8fedeb139c88d2123ec4d
junhyeokahn <junhyeokahn91@gmail.com>
2018-06-26 23:21:14 -0500

    drake binary installation based on system

--- install.sh
+++ install.sh
@@ -1,5 +1,14 @@
-mkdir drake_binary
-cd drake_binary
-curl -o drake.tar.gz https://drake-packages.csail.mit.edu/drake/continuous/drake-latest-mac.tar.gz
-sleep 30
-sudo tar -xvzf drake.tar.gz -C /opt
+
+if [ "$(uname)" == "Darwin" ]; then
+    mkdir drake_binary
+    cd drake_binary
+    curl -o drake.tar.gz https://drake-packages.csail.mit.edu/drake/continuous/drake-latest-mac.tar.gz
+    sleep 30
+    sudo tar -xvzf drake.tar.gz -C /opt
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    mkdir drake_binary
+    cd drake_binary
+    curl -O https://drake-packages.csail.mit.edu/drake/nightly/drake-latest-xenial.tar.gz
+    sleep 30
+    sudo tar -xvzf drake-latest-xenial.tar.gz -C /opt
+fi
