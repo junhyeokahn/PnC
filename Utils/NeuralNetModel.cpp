1d419b24a5775b7aa7ef3554805330ef8984b1bf
junhyeokahn <junhyeokahn91@gmail.com>
2019-02-07 07:09:02 -0600

    nn_model added

--- Utils/NeuralNetModel.cpp
+++ Utils/NeuralNetModel.cpp
@@ -1,5 +1,32 @@
 #include "Utils/NeuralNetModel.hpp"
 
-NeuralNetModel::NeuralNetModel() {}
+Layer::Layer(Eigen::MatrixXd weight, Eigen::VectorXd bias, ActivationFunction act_fn) {
+    weight_ = weight;
+    bias_ = bias;
+    num_input_ = weight.rows();
+    num_output_ = weight.cols();
+    act_fn_ = act_fn;
+}
+
+Layer::~Layer() {}
+
+Eigen::VectorXd Layer::GetOutput(const Eigen::VectorXd & input) {
+    Eigen::VectorXd ret = Eigen::VectorXd::Zero(num_output_);
+
+    return ret;
+}
+
+NeuralNetModel::NeuralNetModel(std::vector<Layer> layers) {
+    layers_ = layers;
+    num_hidden_layer_ = layers.size();
+    num_input_ = layers[0].GetNumInput();
+    num_output_ = layers.back().GetNumOutput();
+}
 
 NeuralNetModel::~NeuralNetModel() {}
+
+Eigen::VectorXd NeuralNetModel::GetOutput( const Eigen::VectorXd & input ) {
+    Eigen::VectorXd ret = Eigen::VectorXd::Zero(num_output_);
+
+    return ret;
+}
--- Utils/NeuralNetModel.hpp
+++ Utils/NeuralNetModel.hpp
@@ -2,12 +2,44 @@
 
 #include "Utils/Utilities.hpp"
 
+/*! \enum ActivationFunction
+ *
+ *  Detailed description
+ */
+enum ActivationFunction { 
+    None = 0,
+    Tanh = 1,
+    ReLU = 2
+};
+
+class Layer
+{
+public:
+    Layer ( Eigen::MatrixXd weight, Eigen::VectorXd bias, ActivationFunction act_fn);
+    virtual ~Layer ();
+    Eigen::VectorXd GetOutput( const Eigen::VectorXd & input );
+    int GetNumInput() { return num_input_; }
+    int GetNumOutput() { return num_output_; }
+
+private:
+    Eigen::MatrixXd weight_;
+    Eigen::VectorXd bias_;
+    int num_input_;
+    int num_output_;
+    ActivationFunction act_fn_;
+};
+
 class NeuralNetModel
 {
 public:
-    NeuralNetModel ();
+    NeuralNetModel (std::vector<Layer> layers);
     virtual ~NeuralNetModel ();
 
+    Eigen::VectorXd GetOutput( const Eigen::VectorXd & input );
+
 private:
-    /* data */
+    int num_input_;
+    int num_output_;
+    int num_hidden_layer_;
+    std::vector<Layer> layers_;
 };
