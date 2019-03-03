32e8a788177d5dff9a4e8e8b862e19045886440e
junhyeokahn <junhyeokahn91@gmail.com>
2019-03-02 21:40:28 -0600

    delete gym

--- ReinforcementLearning/gym/gym/envs/custom/__init__.py
+++
@@ -1,7 +0,0 @@
-# from gym.envs.custom.dummy_cartpole import DummyCartPoleEnv
-# from gym.envs.custom.my_bullet_cartpole import MyBulletCartPoleEnv
-# from gym.envs.custom.my_bullet_humanoid import MyBulletHumanoidEnv
-# from gym.envs.custom.dummy_draco import DummyDracoEnv
-# from gym.envs.custom.draco import DracoEnv
-# from gym.envs.custom.robot_locomotors2 import DracoEnv2
-# from gym.envs.custom.robot_locomotors2 import AtlasEnv
