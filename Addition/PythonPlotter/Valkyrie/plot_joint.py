ce2ec72034acfd1402dc140014fc23241b40b510
junhyeokahn <junhyeokahn91@gmail.com>
2019-08-08 19:05:48 -0500

    jpos plotting added, before adding foot orientation ctrl

--- Addition/PythonPlotter/Valkyrie/plot_joint.py
+++ Addition/PythonPlotter/Valkyrie/plot_joint.py
@@ -21,7 +21,7 @@ np.genfromtxt(file_path+'qdot.txt', delimiter=None, dtype=(float))
 data_jvel_des = \
 np.genfromtxt(file_path+'jvel_des.txt', delimiter=None, dtype=(float))
 
-command = \
+data_command = \
 np.genfromtxt(file_path+'command.txt', delimiter=None, dtype=(float))
 
 data_x = np.genfromtxt(file_path+'time.txt', delimiter='\n', dtype=(float))
@@ -40,32 +40,89 @@ for i in range(0,len(data_x)-1):
 
 axes = plt.gca()
 
-jidx = range(data_jpos_des.shape[1])
+# jidx = range(data_jpos_des.shape[1])
 
-for i in jidx:
-    fig=plt.figure(figure_number)
-    fig.canvas.set_window_title(str(i) + ' th joint')
+# for i in jidx:
+    # fig=plt.figure(figure_number)
+    # fig.canvas.set_window_title(str(i) + ' th joint')
 
-    ax = plt.subplot(3, 1, 1)
-    plt.plot(data_x, data_q[st_idx:end_idx, i+6], "b-")
-    plt.plot(data_x, data_jpos_des[st_idx:end_idx, i], "r-")
+    # ax = plt.subplot(3, 1, 1)
+    # plt.plot(data_x, data_q[st_idx:end_idx, i+6], "b-")
+    # plt.plot(data_x, data_jpos_des[st_idx:end_idx, i], "r-")
+    # plt.grid(True)
+
+    # ax = plt.subplot(3, 1, 2)
+    # plt.plot(data_x, data_qdot[st_idx:end_idx, i+6], "b-")
+    # plt.plot(data_x, data_jvel_des[st_idx:end_idx, i], "r-")
+    # plt.grid(True)
+
+    # ax = plt.subplot(3, 1, 3)
+    # plt.plot(data_x, command[st_idx:end_idx, i], "b-")
+    # plt.grid(True)
+
+    # plt.xlabel('time (sec)')
+
+    # for j in phseChange:
+        # plt.axvline(x=data_x[j],color='indigo',linestyle='-')
+        # plt.text(data_x[j],ax.get_ylim()[1],'%d'%(data_phse[j]),color='indigo')
+
+    # figure_number += 1
+
+left_leg_idx = [0, 1, 2, 3, 4, 5] ## left leg
+right_leg_idx = [6, 7, 8, 9, 10, 11] ## right leg
+n_leg = len(left_leg_idx)
+
+fig=plt.figure(figure_number)
+fig.canvas.set_window_title('left leg jpos(hy, hr, hp, kp, ap, ar)')
+for i in range(1, 7, 1):
+    ax = plt.subplot(6, 1, i)
+    plt.plot(data_x, data_q[st_idx:end_idx, left_leg_idx[i-1]+6], 'b-')
+    plt.plot(data_x, data_jpos_des[st_idx:end_idx, left_leg_idx[i-1]], 'r-')
     plt.grid(True)
+figure_number += 1
 
-    ax = plt.subplot(3, 1, 2)
-    plt.plot(data_x, data_qdot[st_idx:end_idx, i+6], "b-")
-    plt.plot(data_x, data_jvel_des[st_idx:end_idx, i], "r-")
+fig=plt.figure(figure_number)
+fig.canvas.set_window_title('left leg jvel(hy, hr, hp, kp, ap, ar)')
+for i in range(1, 7, 1):
+    ax = plt.subplot(6, 1, i)
+    plt.plot(data_x, data_qdot[st_idx:end_idx, left_leg_idx[i-1]+6], 'b-')
+    plt.plot(data_x, data_jvel_des[st_idx:end_idx, left_leg_idx[i-1]], 'r-')
     plt.grid(True)
+figure_number += 1
 
-    ax = plt.subplot(3, 1, 3)
-    plt.plot(data_x, command[st_idx:end_idx, i], "b-")
+fig=plt.figure(figure_number)
+fig.canvas.set_window_title('left leg command(hy, hr, hp, kp, ap, ar)')
+for i in range(1, 7, 1):
+    ax = plt.subplot(6, 1, i)
+    plt.plot(data_x, data_command[st_idx:end_idx, left_leg_idx[i-1]], 'b-')
     plt.grid(True)
+figure_number += 1
 
-    plt.xlabel('time (sec)')
 
-    for j in phseChange:
-        plt.axvline(x=data_x[j],color='indigo',linestyle='-')
-        plt.text(data_x[j],ax.get_ylim()[1],'%d'%(data_phse[j]),color='indigo')
+fig=plt.figure(figure_number)
+fig.canvas.set_window_title('right leg jpos(hy, hr, hp, kp, ap, ar)')
+for i in range(1, 7, 1):
+    ax = plt.subplot(6, 1, i)
+    plt.plot(data_x, data_q[st_idx:end_idx, right_leg_idx[i-1]+6], 'b-')
+    plt.plot(data_x, data_jpos_des[st_idx:end_idx, right_leg_idx[i-1]], 'r-')
+    plt.grid(True)
+figure_number += 1
 
-    figure_number += 1
+fig=plt.figure(figure_number)
+fig.canvas.set_window_title('right leg jvel(hy, hr, hp, kp, ap, ar)')
+for i in range(1, 7, 1):
+    ax = plt.subplot(6, 1, i)
+    plt.plot(data_x, data_qdot[st_idx:end_idx, right_leg_idx[i-1]+6], 'b-')
+    plt.plot(data_x, data_jvel_des[st_idx:end_idx, right_leg_idx[i-1]], 'r-')
+    plt.grid(True)
+figure_number += 1
+
+fig=plt.figure(figure_number)
+fig.canvas.set_window_title('right leg command(hy, hr, hp, kp, ap, ar)')
+for i in range(1, 7, 1):
+    ax = plt.subplot(6, 1, i)
+    plt.plot(data_x, data_command[st_idx:end_idx, right_leg_idx[i-1]], 'b-')
+    plt.grid(True)
+figure_number += 1
 
 plt.show()
