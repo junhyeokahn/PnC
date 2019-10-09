5c7bfaa8f191dc787d746144e10ef11270a3e40d
junhyeokahn <junhyeokahn91@gmail.com>
2019-10-09 11:54:01 -0500

    merge

--- Script/install_sim.sh
+++ Script/install_sim.sh
@@ -2,14 +2,16 @@
 PATH_PACKAGE="$(pwd)"
 
 if [ "$(uname)" == "Darwin" ]; then
-    echo "not implemented"
+    echo "# ==================================================================="
+    echo "# Install Dart"
+    echo "# ==================================================================="
+    brew install dartsim --only-dependencies &&
+    cd ${PATH_PACKAGE}/.. && git clone https://github.com/junhyeokahn/dart.git && cd dart && git checkout release-6.9 &&
+    mkdir build && cd build && cmake .. && make -j6 && sudo make install
+
+    cd ${PATH_PACKAGE}
+
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-
-    echo "# ==================================================================="
-    echo "# Install PyBullet"
-    echo "# ==================================================================="
-    pip install pybullet
-
     echo "# ==================================================================="
     echo "# Install Dart"
     echo "# ==================================================================="
@@ -23,9 +25,8 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     sudo apt-get install libxi-dev libxmu-dev freeglut3-dev &&
     sudo apt-get install libopenscenegraph-dev &&
     sudo apt-get install liboctomap-dev &&
-    cd ~/Repository/ && git clone https://github.com/junhyeokahn/dart.git && cd dart && git checkout release-6.8 &&
+    cd ${PATH_PACKAGE}/.. && git clone https://github.com/junhyeokahn/dart.git && cd dart && git checkout release-6.9 &&
     mkdir build && cd build && cmake .. && make -j6 && sudo make install
 
     cd ${PATH_PACKAGE}
 fi
-
