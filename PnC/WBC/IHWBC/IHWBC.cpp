ac8f3c17bfa780e44ddc7129c77d5675933e7473
Steven Jens Jorgensen <stevenjensj@gmail.com>
2019-11-21 17:32:33 -0600

    formulated the task cost terms for IHWBC

--- PnC/WBC/IHWBC/IHWBC.cpp
+++ PnC/WBC/IHWBC/IHWBC.cpp
@@ -1,7 +1,8 @@
 #include <PnC/WBC/IHWBC/IHWBC.hpp>
 
 // Constructor
-IHWBC::IHWBC(const std::vector<bool> & act_list): num_act_joint_(0), num_passive_(0) {
+IHWBC::IHWBC(const std::vector<bool> & act_list): 
+	num_act_joint_(0), num_passive_(0), b_updatesetting_(false) {
     // Set Number of degrees of freedom
     num_qdot_ = act_list.size();
     
@@ -17,6 +18,10 @@ IHWBC::IHWBC(const std::vector<bool> & act_list): num_act_joint_(0), num_passive
     // Selection Matrices
     Sa_ = Eigen::MatrixXd::Zero(num_act_joint_, num_qdot_);
     Sv_ = Eigen::MatrixXd::Zero(num_passive_, num_qdot_);
+    Sf_ = Eigen::MatrixXd::Zero(6, num_qdot_);
+
+    // Set Floating Base Selection Matrices
+    Sf_.block(0,0, 6, 6).setIdentity();
 
     // Set virtual & actuated selection matrix
     int j(0);
@@ -32,7 +37,60 @@ IHWBC::IHWBC(const std::vector<bool> & act_list): num_act_joint_(0), num_passive
         }
     }
 
+    // Set Tikhonov regularization for decision variables
+    lambda_qddot = 1e-9;
+	lambda_Fr = 1e-9;
+
 }
 
 // Destructor
 IHWBC::~IHWBC(){}
+
+void IHWBC::updateSetting(const Eigen::MatrixXd & A,
+		                 const Eigen::MatrixXd & Ainv,
+		                 const Eigen::VectorXd & cori,
+		                 const Eigen::VectorXd & grav,
+		                 void* extra_setting){
+    A_ = A;
+    Ainv_ = Ainv;
+    cori_ = cori;
+    grav_ = grav;
+    b_updatesetting_ = true;
+}
+void IHWBC::solve(const std::vector<Task*> & task_list,
+		          const std::vector<ContactSpec*> & contact_list,
+		          const Eigen::VectorXd & Fd,
+		          Eigen::VectorXd & tau_cmd, Eigen::VectorXd & qddot_cmd){
+    if(!b_updatesetting_) { printf("[Warning] IHWBC setting is not done\n"); }
+    // Task Matrices and Vectors
+    Eigen::MatrixXd Jt, Jc; 
+    Eigen::VectorXd JtDotQdot, xddot;
+
+    // Cost Matrices and Vectors
+    Eigen::MatrixXd Pt, Pf; 
+    Eigen::VectorXd vt, vf;
+
+    // Task Cost Matrix
+    Pt = Eigen::MatrixXd::Zero(num_qdot_, num_qdot_);
+	vt = Eigen::VectorXd::Zero(num_qdot_);
+
+	// Equivalent formulation of the task terms:
+    // sum_i=1^n w_i|| J_i*qddot + J_i*dotqdot - x_iddot ||^2_2 + lambda_qddot*||qddot||^2_2
+    Task* task;
+    for(int i = 0; i < task_list.size(); i++){
+    	task = task_list[i];
+    	task->getTaskJacobian(Jt);
+        task->getTaskJacobianDotQdot(JtDotQdot);
+        task->getCommand(xddot);       
+        Pt += (w_task_heirarchy[i]*(Jt.transpose()*Jt));
+        vt += (w_task_heirarchy[i]*(-(JtDotQdot-xddot).transpose()*Jt));
+    }
+    Pt += (lambda_qddot*Eigen::MatrixXd::Identity(num_qdot_, num_qdot_)); 
+
+    // Count dimension of contacts from the contact_list
+    int dim_contacts = 0;
+    
+    
+
+
+}
--- PnC/WBC/IHWBC/IHWBC.hpp
+++ PnC/WBC/IHWBC/IHWBC.hpp
@@ -1,3 +1,4 @@
+#include <PnC/WBC/Task.hpp>
 #include <PnC/WBC/ContactSpec.hpp>
 #include <Utils/IO/IOUtilities.hpp>
 #include <ExternalSource/myOptimizer/Goldfarb/QuadProg++.hh>
@@ -8,18 +9,57 @@ public:
 	IHWBC(const std::vector<bool> & act_list);
     virtual ~IHWBC();
 
-protected:
+    void updateSetting(const Eigen::MatrixXd & A,
+                    const Eigen::MatrixXd & Ainv,
+                    const Eigen::VectorXd & cori,
+                    const Eigen::VectorXd & grav,
+                    void* extra_setting = NULL);
+
+    void solve(const std::vector<Task*> & task_list,
+               const std::vector<ContactSpec*> & contact_list,
+               const Eigen::VectorXd & Fd,                
+               Eigen::VectorXd & tau_cmd, Eigen::VectorXd & qddot_cmd);
+
+    // Weight of task hierarchies
+    Eigen::VectorXd w_task_heirarchy;
+
+    // Weights of contact reaction forces
+    Eigen::VectorXd w_rf_contacts;
+
+    // Tikhonov regularization
+    double lambda_qddot; 
+    double lambda_Fr; 
+
+private:
     int num_qdot_; // Number of degrees of freedom
     int num_act_joint_; // Actuated Joints
     int num_passive_; // Passive Joints
 
+    // Selection Matrices
     Eigen::MatrixXd Sa_; // Actuated joint
     Eigen::MatrixXd Sv_; // Virtual joint
+    Eigen::MatrixXd Sf_; // Floating base 
 
     Eigen::MatrixXd A_;
     Eigen::MatrixXd Ainv_;
     Eigen::VectorXd cori_;
     Eigen::VectorXd grav_;	
+
+    bool b_updatesetting_;
+
+    // Quadprog Variables
+    GolDIdnani::GVect<double> x;
+    // Cost
+    GolDIdnani::GMatr<double> G;
+    GolDIdnani::GVect<double> g0;
+
+    // Equality
+    GolDIdnani::GMatr<double> CE;
+    GolDIdnani::GVect<double> ce0;
+
+    // Inequality
+    GolDIdnani::GMatr<double> CI;
+    GolDIdnani::GVect<double> ci0;
 
 };
 
