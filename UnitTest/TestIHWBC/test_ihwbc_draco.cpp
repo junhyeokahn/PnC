2b64b17bcaaa322a81a17b179b21402f7fbc0cfd
Steven Jens Jorgensen <stevenjensj@gmail.com>
2019-11-22 19:21:07 -0600

    preparing task and contact list for ihwbc small test

--- UnitTest/TestIHWBC/test_ihwbc_draco.cpp
+++ UnitTest/TestIHWBC/test_ihwbc_draco.cpp
@@ -4,9 +4,12 @@
 #include <Eigen/Dense>
 #include <Utils/IO/IOUtilities.hpp>
 #include <PnC/RobotSystem/RobotSystem.hpp>
-
 #include <PnC/WBC/IHWBC/IHWBC.hpp>
 
+// Draco Specific
+#include <PnC/DracoPnC/ContactSet/ContactSet.hpp>
+#include <PnC/DracoPnC/CtrlSet/CtrlSet.hpp>
+#include <PnC/DracoPnC/DracoDefinition.hpp>
 
 void setInitialConfiguration(RobotSystem* & robot) {
     int lKneeIdx = robot->getDofIdx("lKnee");
@@ -38,9 +41,6 @@ TEST(IHWBC, robot) {
     RobotSystem* robot;
     robot = new RobotSystem(6, THIS_COM "RobotModel/Robot/Draco/DracoPnC_Dart.urdf");
 
-    setInitialConfiguration(robot);
-    std::cout << "q:" << robot->getQ().transpose() << std::endl;
-
     //  Initialize actuated list
     std::vector<bool> act_list;
     act_list.resize(robot->getNumDofs(), true);
@@ -50,50 +50,34 @@ TEST(IHWBC, robot) {
 
     // Initialize IHWBC
     IHWBC* ihwbc = new IHWBC(act_list);
+
+    // Task and Contact list
+	std::vector<Task*> task_list;
+	std::vector<ContactSpec*> contact_list;
+
+    // Create Tasks
+    Task* total_joint_task = new BasicTask(robot, BasicTaskType::JOINT, Draco::n_adof);
+    task_list.push_back(total_joint_task);
+
+    // Create the contacts
+    ContactSpec* rfoot_front_contact = new PointContactSpec(robot, DracoBodyNode::rFootFront, 0.7);
+    ContactSpec* rfoot_back_contact = new PointContactSpec(robot, DracoBodyNode::rFootBack, 0.7);
+    ContactSpec* lfoot_front_contact = new PointContactSpec(robot, DracoBodyNode::lFootFront, 0.7);
+    ContactSpec* lfoot_back_contact = new PointContactSpec(robot, DracoBodyNode::lFootBack, 0.7);
+
+    contact_list.push_back(rfoot_front_contact);
+    contact_list.push_back(rfoot_back_contact);
+    contact_list.push_back(lfoot_front_contact);
+    contact_list.push_back(lfoot_back_contact);
+
+    setInitialConfiguration(robot);
+    std::cout << "q:" << robot->getQ().transpose() << std::endl;
+
+	Eigen::MatrixXd A = robot->getMassMatrix();
+	Eigen::MatrixXd Ainv = robot->getInvMassMatrix();
+	Eigen::MatrixXd grav = robot->getGravity();
+	Eigen::MatrixXd coriolis = robot->getCoriolis();
+
+	// myUtils::pretty_print(A, std::cout, "A");
 }
 
-
-// int main(int argc, char ** argv){
-//     RobotSystem* robot;
-//     robot = new RobotSystem(6, THIS_COM "RobotModel/Robot/Draco/DracoPnC_Dart.urdf");
-//     std::cout << "q:" << robot->getQ().transpose() << std::endl;
-// }	
-// #include <dart/dart.hpp>
-// #include <dart/gui/osg/osg.hpp>
-// #include <dart/utils/urdf/urdf.hpp>
-// #include <dart/utils/utils.hpp>
-
-// void setInitialConfiguration(dart::dynamics::SkeletonPtr & robot) {
-//     int lKneeIdx = robot->getDof("lKnee")->getIndexInSkeleton();
-//     int lHipPitchIdx = robot->getDof("lHipPitch")->getIndexInSkeleton();
-//     int rKneeIdx = robot->getDof("rKnee")->getIndexInSkeleton();
-//     int rHipPitchIdx = robot->getDof("rHipPitch")->getIndexInSkeleton();
-//     int lAnkleIdx = robot->getDof("lAnkle")->getIndexInSkeleton();
-//     int rAnkleIdx = robot->getDof("rAnkle")->getIndexInSkeleton();
-
-//     int initPos(2);  // 0 : Home, 1 : Simulation, 2 : Experiment
-//     Eigen::VectorXd q = robot->getPositions();
-
-//     q[2] = 1.193;
-//     double alpha(-M_PI / 4.);
-//     double beta(M_PI / 5.5);
-//     q[lHipPitchIdx] = alpha;
-//     q[lKneeIdx] = beta - alpha;
-//     q[rHipPitchIdx] = alpha;
-//     q[rKneeIdx] = beta - alpha;
-//     q[lAnkleIdx] = M_PI / 2 - beta;
-//     q[rAnkleIdx] = M_PI / 2 - beta;
-
-//     robot->setPositions(q);
-// }
-
-// int main(int argc, char ** argv){
-//     dart::utils::DartLoader urdfLoader;
-//     dart::dynamics::SkeletonPtr robot = urdfLoader.parseSkeleton(
-//         THIS_COM "RobotModel/Robot/Draco/DracoSim_Dart.urdf");
-// 	setInitialConfiguration(robot);
-
-
-//     std::cout << "robot position" << std::endl;
-//     std::cout << robot->getPositions().transpose() << std::endl;
-// }
