c226e196e961f8eea331d04ac413339a32474e90
Will Becker <will@thinkingrobots.ai>
2020-02-10 13:04:03 -0500

    Added some method signatures and comments to help clarify our desires for the DracoInterface; cleaned up some cmake experimentation.

--- PnC/DracoPnC/DracoInterface.hpp
+++ PnC/DracoPnC/DracoInterface.hpp
@@ -70,6 +70,31 @@ class DracoInterface : public EnvInterface {
     void Walk(double ft_length, double r_ft_width, double l_ft_width,
               double ori_inc, int num_step);
 
+    //These would be the new API functionality that we would want for integration into ADE, such that we can build a
+    //system to navigate to a relative point on the ADE side of things.
+
+    //We have two potential ways that we would like to direct the robot to walk in a simpler manner (both being
+    //implemented if possible)
+    //Walk() would simply walk forward forever until receiving a Halt() (described below)
+    void Walk();
+
+    //Walk(int) [or double, or whatever fits best] would walk the robot straight forward for a given distance
+    //That distance could be a standard number of footsteps, or a physical distance in any unit, as long as we are able
+    //to convert the distance to a point measured by ADE to this method's parameters
+    void Walk(int distance);
+
+    //TurnTo would direct the robot to turn in a direction
+    //heading represents a 2 dimensional vector that the robot should face in the direction of.
+    // - This could also be a point struct of some kind.
+    void TurnTo(std::vector<double> heading);
+
+    //Halt would send an interrupt command to stop planning and reset the robot to a safe home/waiting position
+    //We would like to be able to cancel a goal sent to the robot through ADE by having a way for the robot to
+    //halt it's motion. Currently you can send a zero step command to Walk, which will have the robot take a half step
+    //and then stop, but it cannot interrupt as there is no message queuing or interruption in the DracoInterface as of
+    //yet.
+    void Halt();
+
     void GetCoMTrajectory(std::vector<Eigen::VectorXd>& com_des_list);
     void GetContactSequence(std::vector<Eigen::Isometry3d>& foot_target_list);
     bool IsTrajectoryUpdated();
