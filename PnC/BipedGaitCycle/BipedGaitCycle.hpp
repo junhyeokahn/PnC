4e47e65b7d2221e16edc0b5a6779cd97517f1401
Steven Jens Jorgensen <stevenjensj@gmail.com>
2020-02-12 13:55:42 -0600

    begin gait cycle implementation.

---
+++ PnC/BipedGaitCycle/BipedGaitCycle.hpp
@@ -0,0 +1,16 @@
+#ifndef H_BIPED_GAIT_CYCLE
+#define H_BIPED_GAIT_CYCLE
+
+// Standard
+#include <math.h>
+#include <stdio.h>
+#include <iostream>
+
+// Header
+class BipedGaitCycle{
+public: 
+	BipedGaitCycle();
+	~BipedGaitCycle();
+};
+
+#endif
---
+++ PnC/BipedGaitCycle/test_gait_cycle.cpp
@@ -0,0 +1,6 @@
+#include <PnC/BipedGaitCycle/BipedGaitCycle.hpp>
+
+int main(int argc, char** argv) {
+	BipedGaitCycle gait_cycle;
+	return 0;
+}
