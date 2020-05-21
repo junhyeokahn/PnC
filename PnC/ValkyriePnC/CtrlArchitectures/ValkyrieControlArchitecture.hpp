2e1d1bc128831c1fe4d3d06158828dd3d9fdcabd
stevenjj <stevenjensj@gmail.com>
2020-05-21 00:30:33 -0500

    begin controller code refactor.

--- PnC/ValkyriePnC/CtrlArchitectures/ValkyrieControlArchitecture.hpp
+++ PnC/ValkyriePnC/CtrlArchitectures/ValkyrieControlArchitecture.hpp
@@ -4,6 +4,8 @@
 
 #include <PnC/WBC/Task.hpp>
 #include <PnC/WBC/ContactSpec.hpp>
+#include <PnC/WBC/IHWBC/IHWBC.hpp>
+#include <PnC/WBC/IHWBC/IHWBC_JointIntegrator.hpp>
 
 #include <PnC/ValkyriePnC/ValkyrieDefinition.hpp>
 #include <PnC/ValkyriePnC/ValkyrieStateProvider.hpp>
@@ -29,8 +31,76 @@ class ValkyrieControlArchitecture : public ControlArchitecture {
 
     YAML::Node cfg_;
 
-    // Temporary
+    // Temporary -------------
     bool b_first_visit_;
     Controller* balance_ctrl_;
+    // -----------------------
+   
+    public:
+    // -------------------------------------------------------
+    // Controller Objects
+    // -------------------------------------------------------
+    IHWBC* ihwbc_;
+    Eigen::VectorXd tau_cmd_;
+    Eigen::VectorXd qddot_cmd_;
+
+    IHWBC_JointIntegrator* ihwbc_joint_integrator_;
+    Eigen::VectorXd des_jacc_;
+    Eigen::VectorXd des_jpos_;
+    Eigen::VectorXd des_jvel_;
+    // -------------------------------------------------------
+    // Task Member variables
+    // -------------------------------------------------------
+    Task* com_task_;
+    Task* pelvis_ori_task_;
+    Task* ang_momentum_task_;
+    Task* upper_body_task_;
+    std::vector<int> upper_body_joint_indices_; // list of upperbody joint names
+
+    Task* rfoot_center_pos_task;
+    Task* lfoot_center_pos_task;
+    Task* rfoot_center_ori_task;
+    Task* lfoot_center_ori_task;
+
+    // -------------------------------------------------------
+    // Contact Member variables
+    // -------------------------------------------------------    
+    ContactSpec* rfoot_contact_;
+    ContactSpec* lfoot_contact_;
+    double lfoot_max_z_;
+    double rfoot_max_z_;
+
+    // -------------------------------------------------------
+    // Parameters
+    // -------------------------------------------------------
+    
+    // IHWBC Controller parameters
+    double ihwbc_dt_; // Joint integration time
+    double w_contact_weight_; // contact weights hierarchy (relative to task weights)
+    double lambda_qddot_; // Joint acceleration variable normalization
+    double lambda_Fr_; //  Reaction Force normalization
+
+    // Task Hierarchy Weights
+    Eigen::VectorXd w_task_hierarchy_;
+    double w_task_com_;
+    double w_task_ang_mom_;
+    double w_task_upper_body_;
+    double w_task_pelvis_;
+    double w_task_rfoot_;
+    double w_task_lfoot_;
+
+    // Task Gains
+    Eigen::VectorXd kp_com_;
+    Eigen::VectorXd kd_com_;
+    Eigen::VectorXd kp_ang_mom_;
+    Eigen::VectorXd kd_ang_mom_;
+    Eigen::VectorXd kp_pelvis_;
+    Eigen::VectorXd kd_pelvis_;
+    Eigen::VectorXd kp_joint_;
+    Eigen::VectorXd kd_joint_;
+    Eigen::VectorXd kp_upper_body_joint_;
+    Eigen::VectorXd kd_upper_body_joint_;
+    Eigen::VectorXd kp_foot_;
+    Eigen::VectorXd kd_foot_;
 
 };
