2009779fbfa6b41197e69481e48cd17c23f1acbe
Ryan Gupta <ryangupta8@gmail.com>
2021-06-22 11:13:40 -0500

    Preparing to PyBind the normal simulation for Mingyo to use in PyBullet

---
+++ Bindings/PythonTest/main.py
@@ -0,0 +1,39 @@
+import sys
+import os
+sys.path.append('/home/knapsack/project/locomotion/src/PnC/build/lib')
+
+import A1Interface
+import numpy as np
+
+def main():
+    draco_interface = A1Interface.A1Interface()
+    draco_sensor_data = A1Interface.A1SensorData()
+    draco_command = A1Interface.A1Command()
+
+    # draco_sensor_data.imu_ang_vel = np.zeros(3)
+    # draco_sensor_data.imu_acc = np.array([0, 0, -9.81])
+    # draco_sensor_data.q = np.array([0, 0, -0.5, 1.8, 1.03, 0, 0, -0.5, 1.8, 1.03])
+    # draco_sensor_data.qdot = np.zeros(10)
+    # draco_sensor_data.jtrq = np.zeros(10)
+    # draco_sensor_data.temperature = np.zeros(10)
+    # draco_sensor_data.motor_current = np.zeros(10)
+    # draco_sensor_data.bus_voltage = np.zeros(10)
+    # draco_sensor_data.bus_current = np.zeros(10)
+    # draco_sensor_data.rotor_inertia = np.zeros(10)
+    # draco_sensor_data.rfoot_ati = np.zeros(6)
+    # draco_sensor_data.lfoot_ati = np.zeros(6)
+    # draco_sensor_data.rfoot_contact = True
+    # draco_sensor_data.lfoot_contact = True
+    # draco_command.turn_off = False
+    # draco_command.q = np.zeros(10)
+    # draco_command.qdot = np.zeros(10)
+    # draco_command.jtrq = np.zeros(10)
+
+    #for i in range(10):
+        #draco_interface.getCommand(draco_sensor_data, draco_command)
+
+    print("Done")
+
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -23,3 +23,38 @@ $ mkdir build && cd build && cmake..
 $ make -j
 $ ./bin/run_draco
 ```
+
+
+## Python Binding for A1 Quadruped
+
+### Compile the Code
+- We compile the code using Anaconda. We first need to make a python virtual environment
+```
+$ cd PnC/
+$ conda env create -f py37.yml
+$ conda activate py37
+$ mkdir build && cd build && cmake ..
+$ make -j
+```
+### Link the Library Files
+```
+$ cd build/lib
+$ ln -s A1Interface.cpython-37m-x86_64-linux-gnu.so A1Interface.so
+```
+### Update the Python Test Script and Run
+```
+$ cd PnC/Bindings/PythonTest
+$ vi main.py
+```
+Edit the file path in line 3 to match your file path
+```
+$ python main.py
+```
+The file should run, generate the pretty_constructor output of the MPC-WBIC Algorithm and then print out "Done"
+### Using the Code
+As in main.py, to use the code you will need to initialize 3 objects:
+1) A1Interface
+2) A1SensorData
+3) A1Command
+The header file that defines these objects and their members is located in PnC/PnC/A1PnC/A1Interface.hpp
+
