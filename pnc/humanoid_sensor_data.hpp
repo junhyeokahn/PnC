82c89b81217986908c47c7ddf543a09242cc1d2e
Carlos Gonzalez <carlos.gonzalez@utexas.edu>
2022-01-23 01:47:09 -0600

    added imu accelerometer and magnetometer readings

--- pnc/humanoid_sensor_data.hpp
+++ pnc/humanoid_sensor_data.hpp
@@ -9,6 +9,8 @@ public:
     ~HumanoidSensorData();
 
     Eigen::Matrix<double, 6, 1> imu_frame_vel;
+    Eigen::Vector3d imu_accel;
+    Eigen::Vector3d imu_magnet;
     std::map<std::string, double> joint_positions;
     std::map<std::string, double> joint_velocities;
 
