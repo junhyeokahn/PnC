cb928a3bdff720778f4b7fef5a299efb2f889d07
Carlos Gonzalez <carlos.gonzalez@utexas.edu>
2022-02-01 17:17:47 -0600

    added test checking roll angle estimate

--- test/draco_kf_state_estimator_test.cpp
+++ test/draco_kf_state_estimator_test.cpp
@@ -36,13 +36,51 @@ TEST(DracoKFStateEstimatorTest, standingStraight)
   // check error using matrix 2-norm
   Eigen::Matrix3d rotation_error_marg = expectedRotMatrix - estimatedMARGRotMatrix;
   Eigen::Matrix3d rotation_error_imu = expectedRotMatrix - estimatedIMURotMatrix;
-//  std::cout << "Error in MARG rotation matrix: " << std::endl << rotation_error_marg << std::endl;
-//  std::cout << "MARG error norm: " << rotation_error_marg.norm() << std::endl;
-//  std::cout << "Error in IMU rotation matrix: " << std::endl << rotation_error_imu << std::endl;
-//  std::cout << "IMU error norm: " << rotation_error_imu.norm() << std::endl;
   ASSERT_LE((rotation_error_marg).norm(), error_tolerance);
   ASSERT_LE((rotation_error_imu).norm(), error_tolerance);
 }
+
+TEST(DracoKFStateEstimatorTest, roll90Deg)
+{
+  // Note:
+  // the magnetometer measurement obtained for Austin, TX in microTeslas
+  // (https://www.ngdc.noaa.gov/geomag/calculators/magcalc.shtml#igrfwmm)
+  Eigen::Vector3d gyro(0.0, 0.0, 0.0);
+  Eigen::Vector3d accelerometer(0.0, 0.0, 9.8);
+  Eigen::Vector3d magnetometer(24.16, 1.45, 39.98);
+
+  Eigen::Matrix3d expectedRotMatrix;
+  Eigen::Matrix3d estimatedMARGRotMatrix, estimatedIMURotMatrix;
+
+  Eigen::AngleAxis<double> roll90(M_PI_2, Eigen::Vector3d(1., 0., 0.));
+  expectedRotMatrix = roll90.toRotationMatrix();
+  Eigen::Quaternion<double> rollQuat(expectedRotMatrix);
+
+  Eigen::Vector3d rotatedAccelerometer, rotatedMagnetometer;
+  rotatedAccelerometer = roll90.toRotationMatrix().inverse() * accelerometer;
+  rotatedMagnetometer = roll90.toRotationMatrix().inverse() * magnetometer;
+
+  MARGFilter *margFilter = new MARGFilter();
+  margFilter->initialize(rollQuat.w(), rollQuat.x(), rollQuat.y(), rollQuat.z());
+  MARGFilter *imuFilter = new MARGFilter();
+  imuFilter->initialize(rollQuat.w(), rollQuat.x(), rollQuat.y(), rollQuat.z());
+
+  margFilter->filterUpdate(gyro(0), gyro(1), gyro(2),
+                           rotatedAccelerometer(0), rotatedAccelerometer(1), rotatedAccelerometer(2),
+                           rotatedMagnetometer(0), rotatedMagnetometer(1), rotatedMagnetometer(2));
+  estimatedMARGRotMatrix = margFilter->getBaseRotation();
+
+  imuFilter->filterUpdate(gyro(0), gyro(1), gyro(2),
+                          rotatedAccelerometer(0), rotatedAccelerometer(1), rotatedAccelerometer(2));
+  estimatedIMURotMatrix = imuFilter->getBaseRotation();
+
+  // check error using matrix 2-norm
+  Eigen::Matrix3d rotation_error_marg = expectedRotMatrix - estimatedMARGRotMatrix;
+  Eigen::Matrix3d rotation_error_imu = expectedRotMatrix - estimatedIMURotMatrix;
+  ASSERT_LE((rotation_error_marg).norm(), error_tolerance);
+  ASSERT_LE((rotation_error_imu).norm(), error_tolerance);
+}
+
 
 TEST(DracoKFStateEstimatorTest, linearKFZeroNoise)
 {
