b7769945be1bf42caa284b9c570b154ef48d4f24
junhyeokahn <junhyeokahn91@gmail.com>
2022-02-28 07:03:48 -0600

    fix installation script

--- scripts/install/install_protobuf.sh
+++ scripts/install/install_protobuf.sh
@@ -6,16 +6,24 @@ echo '# ==============================================================='
 echo '# installing protobuf'
 echo '# ==============================================================='
 
-sudo apt-get install autoconf automake libtool curl make g++ unzip &&
-cd $PATH_PACKAGE/../ &&
-git clone https://github.com/protocolbuffers/protobuf.git &&
-cd protobuf &&
-git submodule update --init --recursive &&
-./autogen.sh &&
-./configure &&
-make -j4 &&
-make check &&
-sudo make install &&
-sudo ldconfig &&
+if [ "$(uname)" == "Darwin" ]; then
+
+    brew install protobuf
+
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+
+    sudo apt-get install autoconf automake libtool curl make g++ unzip &&
+    cd $PATH_PACKAGE/../ &&
+    git clone https://github.com/protocolbuffers/protobuf.git &&
+    cd protobuf &&
+    git submodule update --init --recursive &&
+    ./autogen.sh &&
+    ./configure &&
+    make -j4 &&
+    make check &&
+    sudo make install &&
+    sudo ldconfig
+
+fi
 
 cd $PATH_PACKAGE
