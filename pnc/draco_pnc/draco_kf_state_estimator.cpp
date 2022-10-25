77391ee8030d377bd3efe49b145714c01e5d4dca
Carlos Gonzalez <carlos.gonzalez@utexas.edu>
2022-10-24 23:11:17 -0500

    fixed some bugs: reset feet position offset after updating once and fixed one IF loop call

--- pnc/draco_pnc/draco_kf_state_estimator.cpp
+++ pnc/draco_pnc/draco_kf_state_estimator.cpp
@@ -101,26 +101,31 @@ void DracoKFStateEstimator::update(DracoSensorData *data) {
 
     // from double to right support and viceversa
     if ((prev_support_state_ == DOUBLE) && (current_support_state_ == RIGHT)) {
-      foot_pos_from_base_pre_transition = x_hat_.tail(6).head(3);
+      foot_pos_from_base_pre_transition = x_hat_.tail(6).head(3);   // estimated lfoot before lift-off
       base_pose_model_.update_leg_covariance(PoseMeasurementModel::LEFT, COV_LEVEL_HIGH);
     } else if ((prev_support_state_ == RIGHT) && (current_support_state_ == DOUBLE)) {
-      foot_pos_from_base_post_transition = robot_->get_link_iso("l_foot_contact").translation();
+      foot_pos_from_base_post_transition = x_hat_.head(3) + robot_->get_link_iso("l_foot_contact").translation();
       global_linear_offset_ = foot_pos_from_base_post_transition - foot_pos_from_base_pre_transition;
-      global_linear_offset_.z() = 0.0;    // TODO make more robust later for non-flat ground
-//      system_model_.update_rfoot_offset(global_linear_offset_);
+//      global_linear_offset_.z() = 0.0;    // TODO make more robust later for non-flat ground
+      system_model_.update_lfoot_offset(global_linear_offset_);
+      foot_pos_from_base_post_transition.z() = 0.0;    // TODO make more robust later for non-flat ground
+//      system_model_.update_lfoot_offset(foot_pos_from_base_post_transition);
+//      base_pose_model_.update_lfoot_offset(foot_pos_from_base_post_transition);
       base_pose_model_.update_leg_covariance(PoseMeasurementModel::LEFT, COV_LEVEL_LOW);
     } else if ((prev_support_state_ == DOUBLE) && (current_support_state_ == LEFT)) {
       // from double support to left support and viceversa
-      foot_pos_from_base_pre_transition = x_hat_.tail(3);
+      foot_pos_from_base_pre_transition = x_hat_.tail(3);   // estimated rfoot before lift-off
       base_pose_model_.update_leg_covariance(PoseMeasurementModel::RIGHT, COV_LEVEL_HIGH);
     } else if ((prev_support_state_ == LEFT) && (current_support_state_ == DOUBLE)) {
-      foot_pos_from_base_post_transition = robot_->get_link_iso("r_foot_contact").translation();
+      foot_pos_from_base_post_transition = x_hat_.head(3) + robot_->get_link_iso("r_foot_contact").translation();
       global_linear_offset_ = foot_pos_from_base_post_transition - foot_pos_from_base_pre_transition;
-      global_linear_offset_.z() = 0.0;  // TODO make more robust later for non-flat ground
-//      system_model_.update_lfoot_offset(global_linear_offset_);
+//      global_linear_offset_.z() = 0.0;  // TODO make more robust later for non-flat ground
+      system_model_.update_rfoot_offset(global_linear_offset_);
+      foot_pos_from_base_post_transition.z() = 0.0;  // TODO make more robust later for non-flat ground
+//      system_model_.update_rfoot_offset(foot_pos_from_base_post_transition);
       base_pose_model_.update_leg_covariance(PoseMeasurementModel::RIGHT, COV_LEVEL_LOW);
     }
-    if (!foot_pos_from_base_post_transition.hasNaN()) {
+    if (foot_pos_from_base_post_transition.hasNaN()) {
       x_hat_ = kalman_filter_.predict(system_model_, accelerometer_input_);
       global_linear_offset_.setZero();
       system_model_.update_lfoot_offset(global_linear_offset_);
@@ -128,10 +133,14 @@ void DracoKFStateEstimator::update(DracoSensorData *data) {
       b_skip_prediction = true;
     }
   }
-  if(!b_skip_prediction)
+  if(!b_skip_prediction) {
     x_hat_ = kalman_filter_.predict(system_model_, accelerometer_input_);
-  else
+    Eigen::Vector3d zeros3d = Eigen::Vector3d::Zero();
+    system_model_.update_lfoot_offset(zeros3d);
+    system_model_.update_rfoot_offset(zeros3d);
+  } else {
     b_skip_prediction = false;
+  }
 
   // update measurement assuming at least one foot is on the ground
   if (sp_->b_lf_contact) {
