04febb991d5ae313033f310caa99e0898b642839
shbang91 <bangsh0718@gmail.com>
2022-11-04 09:59:55 -0500

    forgot to add draco hand reaching state machine

---
+++ pnc/draco_pnc/draco_state_machine/hand_reaching.cpp
@@ -0,0 +1,77 @@
+#include "pnc/draco_pnc/draco_state_machine/hand_reaching.hpp"
+
+HandReaching::HandReaching(const StateIdentifier _state_identifier_in,
+                           DracoControlArchitecture *_ctrl_arch,
+                           RobotSystem *_robot)
+    : StateMachine(_state_identifier_in, _robot), duration_(0.),
+      rel_target_pos_(Eigen::Vector3d::Zero()),
+      rel_target_ori_(Eigen::Quaterniond::Identity()),
+      b_trigger_return_(false) {
+  util::PrettyConstructor(2, "HandReaching");
+
+  ctrl_arch_ = _ctrl_arch;
+  sp_ = DracoStateProvider::getStateProvider();
+}
+
+void HandReaching::firstVisit() {
+  Eigen::Vector3d target_pos = Eigen::Vector3d::Zero();
+  Eigen::Quaterniond target_ori = Eigen::Quaterniond::Identity();
+
+  if (state_identity_ == draco_states::kLHandReaching) {
+    std::cout << "draco_states::kLHandReaching" << std::endl;
+    target_pos =
+        robot_->get_link_iso("l_hand_contact").translation() + rel_target_pos_;
+    target_ori =
+        Eigen::Quaterniond(robot_->get_link_iso("l_hand_contact").linear()) *
+        rel_target_ori_;
+
+    ctrl_arch_->lhand_tm->InitializeInterpolationTrajectory(
+        sp_->curr_time, duration_, target_pos, target_ori);
+
+    // initialize task hierarchy manager
+    ctrl_arch_->lhand_pos_hm->InitializeRampToMax(sp_->curr_time,
+                                                  duration_ / 2.);
+    ctrl_arch_->lhand_ori_hm->InitializeRampToMax(sp_->curr_time,
+                                                  duration_ / 2.);
+
+  } else if (state_identity_ == draco_states::kRHandReaching) {
+    std::cout << "draco_states::kRHandReaching" << std::endl;
+    target_pos =
+        robot_->get_link_iso("r_hand_contact").translation() + rel_target_pos_;
+    target_ori =
+        Eigen::Quaterniond(robot_->get_link_iso("r_hand_contact").linear()) *
+        rel_target_ori_;
+
+    ctrl_arch_->rhand_tm->InitializeInterpolationTrajectory(
+        sp_->curr_time, duration_, target_pos, target_ori);
+
+    // initialize task hierarchy manager
+    ctrl_arch_->rhand_pos_hm->InitializeRampToMax(sp_->curr_time,
+                                                  duration_ / 2.);
+    ctrl_arch_->rhand_ori_hm->InitializeRampToMax(sp_->curr_time,
+                                                  duration_ / 2.);
+  } else
+    std::cout << "[[Error]] no matching draco state machine" << std::endl;
+}
+
+void HandReaching::oneStep() {
+  if (state_identity_ == draco_states::kLHandReaching) {
+    ctrl_arch_->lhand_tm->UpdateDesired(sp_->curr_time);
+    ctrl_arch_->lhand_pos_hm->UpdateRampToMax(sp_->curr_time);
+    ctrl_arch_->lhand_ori_hm->UpdateRampToMax(sp_->curr_time);
+
+  } else if (state_identity_ == draco_states::kRHandReaching) {
+    ctrl_arch_->rhand_tm->UpdateDesired(sp_->curr_time);
+    ctrl_arch_->rhand_pos_hm->UpdateRampToMax(sp_->curr_time);
+    ctrl_arch_->rhand_ori_hm->UpdateRampToMax(sp_->curr_time);
+  }
+}
+
+void HandReaching::lastVisit() {}
+
+bool HandReaching::endOfState() { b_trigger_return_ == true ? true : false; }
+
+StateIdentifier HandReaching::getNextState() {
+  // TODO:
+  // adding hand return state
+}
---
+++ pnc/draco_pnc/draco_state_machine/hand_reaching.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "pnc/draco_pnc/draco_control_architecture.hpp"
+#include "pnc/draco_pnc/draco_state_provider.hpp"
+#include "pnc/state_machine.hpp"
+
+class HandReaching : public StateMachine {
+public:
+  HandReaching(const StateIdentifier _state_identifier_in,
+               DracoControlArchitecture *_ctrl_arch, RobotSystem *_robot);
+  ~HandReaching() = default;
+
+  void oneStep();
+  void firstVisit();
+  void lastVisit();
+  bool endOfState();
+  StateIdentifier getNextState();
+
+  void setDuration(double _duration) { duration_ = _duration; }
+  void setRelTargetPos(const Eigen::Vector3d &pos) { rel_target_pos_ = pos; }
+  void setRelTargetOri(const Eigen::Quaterniond &ori) { rel_target_ori_ = ori; }
+
+protected:
+  DracoStateProvider *sp_;
+  DracoControlArchitecture *ctrl_arch_;
+
+  double duration_;
+  Eigen::Vector3d rel_target_pos_;
+  Eigen::Quaterniond rel_target_ori_;
+
+  bool b_trigger_return_;
+};
