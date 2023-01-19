8c3b3bf7e3c7900c5ed3569787e3de6a2a3f21aa
Carlos Gonzalez <carlos.gonzalez@utexas.edu>
2023-01-19 11:03:59 -0600

    cleanup and moved helper methods to separate meshcat_utils

---
+++ plot/meshcat_utils.py
@@ -0,0 +1,83 @@
+import numpy as np
+
+from pinocchio.visualize import MeshcatVisualizer
+import meshcat.geometry as g
+import meshcat.transformations as tf
+
+import pinocchio as pin
+
+
+def add_arrow(meshcat_visualizer, obj_name, color=[1, 0, 0], height=0.1):
+    arrow_shaft = g.Cylinder(height, 0.01)
+    arrow_head = g.Cylinder(0.04, 0.04, radiusTop=0.001, radiusBottom=0.04)
+    material = g.MeshPhongMaterial()
+    material.color = int(color[0] * 255) * 256**2 + int(color[1] * 255) * 256 + int(color[2] * 255)
+
+    meshcat_visualizer[obj_name].set_object(arrow_shaft, material)
+    meshcat_visualizer[obj_name]["head"].set_object(arrow_head, material)
+
+
+def add_sphere(parent_visualizer, node_name="sphere",
+               urdf_path="robot_model/ground/sphere.urdf",
+               visuals_path="robot_model/ground", color=[0., 0., 1., 0.5]):
+    sphere_model, sphere_collision_model, sphere_visual_model = pin.buildModelsFromUrdf(
+        urdf_path, visuals_path, pin.JointModelFreeFlyer())
+    sphere_viz = MeshcatVisualizer(sphere_model, sphere_collision_model, sphere_visual_model)
+    sphere_viz.initViewer(parent_visualizer)
+    sphere_viz.loadViewerModel(rootNodeName=node_name, color=color)
+
+    return sphere_viz, sphere_model
+
+
+def set_grf_default_position(meschat_visualizer, foot_position):
+    arrow_height = np.array([0, 0, 0.05])
+    arrow_head_offset = np.array([0, arrow_height[2]+0.02/2, 0.0])
+    T_rot = tf.rotation_matrix(np.pi/2, [1, 0, 0])
+    T_trans = tf.translation_matrix(foot_position + arrow_height)
+    T_trans_arrow_head = tf.translation_matrix(arrow_head_offset)
+
+    # first translate, then rotate
+    T = tf.concatenate_matrices(T_trans, T_rot)
+    meschat_visualizer.set_transform(T)
+    meschat_visualizer["head"].set_transform(T_trans_arrow_head)
+
+
+def get_rpy_from_world_to(foot_grf):
+    foot_grf_normalized = foot_grf[3:] / np.linalg.norm(foot_grf[3:])
+    pitch = -np.arcsin(foot_grf_normalized[1])
+    roll = np.arctan2(foot_grf_normalized[0], foot_grf_normalized[2])
+    return np.array([roll, pitch, 0.])
+
+
+def grf_display(meshcat_visualizer, foot_pos, foot_ori, foot_grf):
+    # scale length
+    scale = foot_grf[5] / 100.      # 200 is about half weight
+    S = tf.identity_matrix()
+    S[1, 1] = scale     # y-axis corresponds to height (i.e., length) of cylinder
+
+    # translate and rotate GRF vectors
+    arrow_height = np.array([0, 0, (0.1*scale)/2.])
+    arrow_head_offset = np.array([0, 0.1/2, 0.0])
+    # arrow_head_offset = np.array([0, arrow_height[2]+0.02/2, 0.0])
+    T_arrow_vertical = tf.rotation_matrix(np.pi/2, [1, 0, 0])
+    grf_ori = get_rpy_from_world_to(foot_grf)
+    T_grf_ori = tf.euler_matrix(grf_ori[0], grf_ori[1], grf_ori[2])
+    T_trans = tf.translation_matrix(foot_pos + arrow_height)
+    T_trans_arrow_head = tf.translation_matrix(arrow_head_offset)
+
+    # first translate, then rotate, and scale
+    T = tf.concatenate_matrices(T_trans, T_grf_ori, T_arrow_vertical, S)
+    meshcat_visualizer.set_transform(T)
+    meshcat_visualizer["head"].set_transform(T_trans_arrow_head)
+
+
+def display_visualizer_frames(meshcat_visualizer, frame):
+    for visual in meshcat_visualizer.visual_model.geometryObjects:
+        # Get mesh pose.
+        M = meshcat_visualizer.visual_data.oMg[meshcat_visualizer.visual_model.getGeometryId(visual.name)]
+        # Manage scaling
+        scale = np.asarray(visual.meshScale).flatten()
+        S = np.diag(np.concatenate((scale, [1.0])))
+        T = np.array(M.homogeneous).dot(S)
+        # Update viewer configuration.
+        frame[meshcat_visualizer.getViewerNodeName(visual, pin.GeometryType.VISUAL)].set_transform(T)
--- plot/robot_visualizer.py
+++ plot/robot_visualizer.py
@@ -12,77 +12,11 @@ from pinocchio.visualize import MeshcatVisualizer
 import pinocchio as pin
 
 # Python-Meshcat
-import meshcat.geometry as g
-import meshcat.transformations as tf
 from meshcat.animation import Animation
+from plot import meshcat_utils as vis_tools
 
 cwd = os.getcwd()
 sys.path.append(cwd)
-
-
-def add_arrow(meschat_visualizer, obj_name, meshColor=[1, 0, 0]):
-    arrow_shaft = g.Cylinder(0.1, 0.01)
-    arrow_head = g.Cylinder(0.04, 0.04, radiusTop=0.001, radiusBottom=0.04)
-    material = meshcat.geometry.MeshPhongMaterial()
-    material.color = int(meshColor[0] * 255) * 256**2 + int(meshColor[1] * 255) * 256 + int(meshColor[2] * 255)
-
-    meschat_visualizer[obj_name].set_object(arrow_shaft, material)
-    meschat_visualizer[obj_name]["head"].set_object(arrow_head, material)
-
-
-def set_grf_default_position(meschat_visualizer, foot_position):
-    arrow_height = np.array([0, 0, 0.05])
-    arrow_head_offset = np.array([0, arrow_height[2]+0.02/2, 0.0])
-    T_rot = tf.rotation_matrix(np.pi/2, [1, 0, 0])
-    T_trans = tf.translation_matrix(foot_position + arrow_height)
-    T_trans_arrow_head = tf.translation_matrix(arrow_head_offset)
-
-    # first translate, then rotate
-    T = tf.concatenate_matrices(T_trans, T_rot)
-    meschat_visualizer.set_transform(T)
-    meschat_visualizer["head"].set_transform(T_trans_arrow_head)
-
-def get_rpy_from_world_to(foot_grf):
-    foot_grf_normalized = foot_grf[3:] / np.linalg.norm(foot_grf[3:])
-    pitch = -np.arcsin(foot_grf_normalized[1])
-    roll = np.arctan2(foot_grf_normalized[0], foot_grf_normalized[2])
-    return np.array([roll, pitch, 0.])
-
-def grf_display(meshcat_visualizer, foot_pos, foot_ori, foot_grf):
-    # scale length
-    scale = foot_grf[5] / 100.      # 200 is about half weight
-    S = tf.identity_matrix()
-    S[1, 1] = scale     # y-axis corresponds to height (i.e., length) of cylinder
-
-    # translate and rotate GRF vectors
-    arrow_height = np.array([0, 0, (0.1*scale)/2.])
-    arrow_head_offset = np.array([0, 0.1/2, 0.0])
-    # arrow_head_offset = np.array([0, arrow_height[2]+0.02/2, 0.0])
-    T_arrow_vertical = tf.rotation_matrix(np.pi/2, [1, 0, 0])
-    T_foot_rot = tf.euler_matrix(foot_ori[0], foot_ori[1], foot_ori[2])
-    grf_ori = get_rpy_from_world_to(foot_grf)
-    T_grf_ori = tf.euler_matrix(grf_ori[0], grf_ori[1], grf_ori[2])
-    T_trans = tf.translation_matrix(foot_pos + arrow_height)
-    T_trans_arrow_head = tf.translation_matrix(arrow_head_offset)
-
-    # first translate, then rotate, and scale
-    # T = tf.concatenate_matrices(T_trans, T_foot_rot, T_arrow_vertical, S)
-    T = tf.concatenate_matrices(T_trans, T_grf_ori, T_arrow_vertical, S)
-    meshcat_visualizer.set_transform(T)
-    meshcat_visualizer["head"].set_transform(T_trans_arrow_head)
-
-
-def display_visualizer_frames(meshcat_visualizer):
-    for visual in meshcat_visualizer.visual_model.geometryObjects:
-        # Get mesh pose.
-        M = meshcat_visualizer.visual_data.oMg[meshcat_visualizer.visual_model.getGeometryId(visual.name)]
-        # Manage scaling
-        scale = np.asarray(visual.meshScale).flatten()
-        S = np.diag(np.concatenate((scale, [1.0])))
-        T = np.array(M.homogeneous).dot(S)
-        # Update viewer configuration.
-        frame[meshcat_visualizer.getViewerNodeName(visual, pin.GeometryType.VISUAL)].set_transform(T)
-
 
 # variables to load/display on Meshcat
 exp_time = []
@@ -103,7 +37,6 @@ model, collision_model, visual_model = pin.buildModelsFromUrdf(
     cwd + "/robot_model/draco/draco.urdf", cwd + "/robot_model/draco",
     pin.JointModelFreeFlyer())
 viz = MeshcatVisualizer(model, collision_model, visual_model)
-# viz = meshcat.Visualizer(model, collision_model, visual_model)
 try:
     viz.initViewer(open=True)
 except ImportError as err:
@@ -112,64 +45,30 @@ except ImportError as err:
     )
     print(err)
     sys.exit(0)
-# obj = loadViewerModel(cwd + "/robot_model/draco")
-# viz.set_object(obj)
-viz.loadViewerModel()
+viz.loadViewerModel(rootNodeName="draco")
 vis_q = pin.neutral(model)
 
-com_des_model, com_des_collision_model, com_des_visual_model = pin.buildModelsFromUrdf(
-    "robot_model/ground/sphere.urdf", "robot_model/ground",
-    pin.JointModelFreeFlyer())
-com_des_viz = MeshcatVisualizer(com_des_model, com_des_collision_model, com_des_visual_model)
-com_des_viz.initViewer(viz.viewer)
-com_des_viz.loadViewerModel(rootNodeName="com_des", color=[0., 0., 1., 0.5])
+# add other visualizations to viewer
+com_des_viz, com_des_model = vis_tools.add_sphere(viz.viewer, "com_des", color=[0., 0., 1., 0.5])
 com_des_viz_q = pin.neutral(com_des_model)
 
-com_model, com_collision_model, com_visual_model = pin.buildModelsFromUrdf(
-    "robot_model/ground/sphere.urdf", "robot_model/ground",
-    pin.JointModelFreeFlyer())
-com_viz = MeshcatVisualizer(com_model, com_collision_model, com_visual_model)
-com_viz.initViewer(viz.viewer)
-com_viz.loadViewerModel(rootNodeName="com", color=[1., 0., 0., 0.5])
+com_viz, com_model = vis_tools.add_sphere(viz.viewer, "com", color=[1., 0., 0., 0.5])
 com_viz_q = pin.neutral(com_model)
 
-
-com_proj_model, com_proj_collision_model, com_proj_visual_model = pin.buildModelsFromUrdf(
-    "robot_model/ground/sphere.urdf", "robot_model/ground",
-    pin.JointModelFreeFlyer())
-com_proj_viz = MeshcatVisualizer(com_proj_model, com_proj_collision_model,
-                                 com_proj_visual_model)
-com_proj_viz.initViewer(viz.viewer)
-com_proj_viz.loadViewerModel(rootNodeName="com_proj", color=[0., 0., 1., 0.3])
+com_proj_viz, com_proj_model = vis_tools.add_sphere(viz.viewer, "com_proj", color=[0., 0., 1., 0.3])
 com_proj_viz_q = pin.neutral(com_proj_model)
 
-# create arrows
-add_arrow(viz.viewer, "grf_lf", meshColor=[0, 0, 1])
-add_arrow(viz.viewer, "grf_rf", meshColor=[1, 0, 0])
-# viz.viewer["grf_arrow"].set_object(g.StlMeshGeometry.from_file(cwd+"/robot_model/ground/arrow.urdf"))
+# add arrows visualizers to viewer
+arrow_viz = meshcat.Visualizer(window=viz.viewer.window)
+vis_tools.add_arrow(arrow_viz, "grf_lf", color=[0, 0, 1])
+vis_tools.add_arrow(arrow_viz, "grf_rf", color=[1, 0, 0])
 
-# transform arrows
-lfoot_pos = np.array([0, 0.11, 0])
-rfoot_pos = np.array([0, -0.11, 0])
-set_grf_default_position(viz.viewer["grf_lf"], lfoot_pos)
-set_grf_default_position(viz.viewer["grf_rf"], rfoot_pos)
-# viz.viewer["grf_rf"].set_transform(tf.translation_matrix([0, 1, 0]))
-
-# arrow_model, arrow_collision_model, arrow_visual_model = pin.buildModelsFromUrdf(
-#     "robot_model/ground/arrow.urdf", "robot_model/ground",
-#     pin.JointModelFreeFlyer())
-# arrow_viz = MeshcatVisualizer(arrow_model, arrow_collision_model, arrow_visual_model)
-# arrow_viz.initViewer(viz.viewer)
-# arrow_viz.loadViewerModel(rootNodeName="arrow", color=[0.5, 0.5, 0.5, 0.5])
-# arrow_viz_q = pin.neutral(arrow_model)
-
+# load pkl data
 with open('experiment_data/pnc.pkl', 'rb') as file:
     while True:
         try:
             d = pickle.load(file)
             exp_time.append(d['time'])
-            # base_orientation.append(d['base_joint_quat'])
-            # base_position.append(d['base_joint_pos'])
             lfoot_position.append(d['task_lfoot_lin_pos'])
             rfoot_position.append(d['task_rfoot_lin_pos'])
             lfoot_orientation.append(d['task_lfoot_ori_pos'])
@@ -186,7 +85,7 @@ with open('experiment_data/pnc.pkl', 'rb') as file:
         except EOFError:
             break
 
-# replay data
+# replay data and create animation
 save_freq = 50          # hertz
 anim = Animation(default_framerate=800 / save_freq)
 frame_index = 0
@@ -209,24 +108,24 @@ for ti in range(len(exp_time)):
     com_viz.display(com_viz_q)
 
     # plot GRFs
-    grf_display(viz.viewer["grf_lf"], lfoot_position[ti], lfoot_orientation[ti], lfoot_grf[ti])
-    grf_display(viz.viewer["grf_rf"], rfoot_position[ti], rfoot_orientation[ti], rfoot_grf[ti])
+    vis_tools.grf_display(arrow_viz["grf_lf"], lfoot_position[ti], lfoot_orientation[ti], lfoot_grf[ti])
+    vis_tools.grf_display(arrow_viz["grf_rf"], rfoot_position[ti], rfoot_orientation[ti], rfoot_grf[ti])
 
     # make animation
     with anim.at_frame(viz.viewer, frame_index) as frame:
-        grf_display(frame["grf_lf"], lfoot_position[ti], lfoot_orientation[ti], lfoot_grf[ti])
-        grf_display(frame["grf_rf"], rfoot_position[ti], rfoot_orientation[ti], rfoot_grf[ti])
+        vis_tools.grf_display(frame["grf_lf"], lfoot_position[ti], lfoot_orientation[ti], lfoot_grf[ti])
+        vis_tools.grf_display(frame["grf_rf"], rfoot_position[ti], rfoot_orientation[ti], rfoot_grf[ti])
 
         # save other visualizations at current frame
-        display_visualizer_frames(viz)                  # robot
-        display_visualizer_frames(com_proj_viz)         # projected CoM
-        display_visualizer_frames(com_des_viz)          # desired CoM position
-        display_visualizer_frames(com_viz)              # actual CoM position
+        vis_tools.display_visualizer_frames(viz, frame)                  # robot
+        vis_tools.display_visualizer_frames(com_proj_viz, frame)         # projected CoM
+        vis_tools.display_visualizer_frames(com_des_viz, frame)          # desired CoM position
+        vis_tools.display_visualizer_frames(com_viz, frame)              # actual CoM position
 
 
     frame_index = frame_index + 1
 
 print("Experiment initial time: ", exp_time[0])
 print("Experiment final time: ", exp_time[-1])
-viz.viewer.set_animation(anim)
+viz.viewer.set_animation(anim, play=False)
 
